6add078e9109c50fa283ec7620ec92539efc22f7
p8ul <pkinuthia10@gmail.com>
2018-09-12 02:39:40 +0300

    [Feature #160098501] Allow users to upvote/downvote on an answer

--- src/Components/questionAnswers/actions.js
+++ src/Components/questionAnswers/actions.js
@@ -1,87 +1,58 @@
 import api from '../../utils/api';
 import { getToken } from '../../store';
-import { $on, 
+import {
 	formValidator, 
 	processListErrors,
 	resetQuestionAndAnswersDom 
 } from '../../utils';
+import { 
+	HandleCommentEvents, 
+	HandleVotesEvents
+} from './Events';
 /** Renders */
 import {
 	renderAnswerContent, renderAnswerHeader, 
 	renderQuestionBody,
 	renderQuestionTitle
 } from './TemplateRenders';
-
-const HandleCommentEvents = () => {
-	const commentBtns = document.querySelectorAll('.commentBtn');
-	const commentTextArea = document.querySelectorAll('.comment-box');
-	Object.values(commentBtns).map((el, index) => {		
-		$on(commentBtns[index], 'click',(e)=>{sendComment(e, commentBtns[index]);});
-		$on(commentTextArea[index], 'input',(e)=>{showCommentButton(e, commentBtns[index]);});
-	});
+/**
+ * Call all event listen handlers
+ *
+ */
+const callAllEvents = () => {
+	HandleVotesEvents();
+	HandleCommentEvents();
 };
 
-const showCommentButton = (e, el) => {
-	let id = el.getAttribute('data-id');
-	let commentBtn = document.getElementById(`commentBtn${id}`);
-	commentBtn.classList.remove('hidden');
-};
-
-const resetInnerText = (el, oldText, newText) => {
-	el.innerText = newText;
-	el.classList.add('bg-red');
-	el.classList.add('stretchLeft');
-	setTimeout(()=> {
-		el.innerText = oldText;
-		el.classList.remove('bg-red');
-		el.classList.remove('stretchLeft');
-	}, 2000);
-};
-const sendComment = (e, el) => {
-	let id = el.getAttribute('data-id');
-	let commentText = document.getElementById(`commentBody${id}`).value;	
-	console.error(id, commentText);
-	console.log(el.innerText);
-	if (!commentText) {
-		resetInnerText(el, el.innerText, 'Please add a comment');
-	} else {
-		el.innerText = 'Sending ....';
-		el.classList.add('floating');
-		resetInnerText(el, el.innerText, 'Ok');
-        
-		postComment({
-			url: `questions/answers/comment/${id}`,
-			data: {'comment_body': commentText}
-		});
-	}
-};
-
+/**
+ * Fetch all questions from the server,
+ * Render templates based on result data and
+ * Call all event listen handlers
+ *
+ * @param {!string} url url endpoint
+ */
 export const getQuestion = (url) => {
 	api.get(url)
 		.then(res => res.json())
 		.then(data => data.results)
 		.then(data => {
-			console.log(data.comments);
 			renderQuestionBody(data.question[0]);
 			renderQuestionTitle(data.question[0]);
 			renderAnswerHeader(data.question[0]);
 			renderAnswerContent(data.answers, data.comments);
-			setTimeout(HandleCommentEvents(),3000);
+			setTimeout(callAllEvents(),3000);
 			resetQuestionAndAnswersDom();
 		})
 		.catch(error => console.error(error));
 };
 
-const postComment = ({url, data}) => {
-	api
-		.post(url, data, getToken())
-		.then(res => res.json())
-		.then(data => {
-			window.location.reload();
-		})
-		.catch(error => {console.error(error);});
-};
-
+/**
+ * Valid Answer json object & and Post a request
+ *
+ * @param {!string} event form submit request
+ * @param {!string} url post answer api endpoint
+ * @param {!function} callBackFunc call back function to call after successful request
+ */
 export const sendAnswer = ({event, url, data, callBackFunc}) => {
 	event.preventDefault();
 	let errors = formValidator(data);		
--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,3 +1,22 @@
+import api from './api';
+import { getToken } from '../store';
+
 export const $on = (target, type, callback) => {
 	target.addEventListener(type, callback);
+};
+
+/**
+ * Post and redirect
+ *
+ * @param {!string} url url endpoint
+ * @param {!object} data post object data
+ */
+export const postAndRedirect = ({url, data}) => {
+	api
+		.post(url, data, getToken())
+		.then(res => res.json())
+		.then(() => {
+			window.location.reload();
+		})
+		.catch(error => {console.error(error);});
 };

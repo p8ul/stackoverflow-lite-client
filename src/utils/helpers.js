dd663fe33df783ec497214949b104718888f5786
p8ul <pkinuthia10@gmail.com>
2018-09-12 03:20:12 +0300

    [Feature #160098501] Allow users to upvote/downvote on an answer

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -20,3 +20,28 @@ export const postAndRedirect = ({url, data}) => {
 		})
 		.catch(error => {console.error(error);});
 };
+
+/**
+ * Toggle an element innerHtml content.
+ *
+ * @param {!string} el Html Element clicked (up or down arrow )
+ * @param {!string} oldText Current element innerHtml content
+ * @param {!string} newText new innerHtml Content
+ */
+export const toggleInnerText = (el, oldText, newText) => {
+	el.innerText = newText;
+	el.classList.add('bg-red');
+	el.classList.add('stretchLeft');
+	setTimeout(()=> {
+		el.innerText = oldText;
+		el.classList.remove('bg-red');
+		el.classList.remove('stretchLeft');
+	}, 2000);
+};
+
+export const changeInnerText = (el, newText) => {
+	el.innerText = newText;
+	el.classList.add('bg-red');
+	el.classList.add('stretchLeft');
+	return el;
+};

2d05982878792fd71024cde165fca35f7a3f555b
p8ul <pkinuthia10@gmail.com>
2018-09-21 08:46:58 +0300

    [ft #160635257] Fix failing  permission test

--- src/utils/__tests__/utils.permissoin.test.js
+++ src/utils/__tests__/utils.permissoin.test.js
@@ -10,5 +10,6 @@ const initialState = TEST_SELETED_QUESTION_PAYLOAD;
 test('Test if logged in user can accept question ', () => {
 	setToken(TEST_TOKEN, TEST_TOKEN_KEY);
 	selectedQuestion({type: SET_QUESTION, payload: initialState.question});
-	expect(canAcceptAnswer(TEST_TOKEN_KEY)).toEqual(true);
+	let username = selectedQuestion().question.username;
+	expect(canAcceptAnswer(username, TEST_TOKEN_KEY)).toEqual(true);
 });
